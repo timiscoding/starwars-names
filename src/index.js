b92fd8a0ee8e73217e2e1eacb6487180293d53b7
Tim <timiscoding@gmail.com>
2018-04-05 13:37:29 +1000

    refactor: Add ES6 support
    Because we want cleaner code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-var uniqueRandomArray = require('unique-random-array');
-var starWarsNames = require('./starwars-names.json');
+import uniqueRandomArray from 'unique-random-array';
+import starWarsNames from './starwars-names.json';
 var getRandomItem = uniqueRandomArray(starWarsNames);
 var random = (number=1) => number === 1 ? getRandomItem() : Array(number).map(getRandomItem);
 
